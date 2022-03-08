a3febe12fe16b65d2cb6d4c6a13230218995abaa
meher12 <meher.khemiri@gmail.com>
2022-03-08 14:50:25 +0100

    Add Role scrum master

---
+++ src/app/component/sprint/sprint-list/sprint-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-list',
+  templateUrl: './sprint-list.component.html',
+  styleUrls: ['./sprint-list.component.css']
+})
+export class SprintListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
