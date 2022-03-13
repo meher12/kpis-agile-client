48af94f5f170a2ac52e68300cab5e5a8d30f23d2
meher12 <meher.khemiri@gmail.com>
2022-03-13 12:48:26 +0100

    update story task models

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -1,15 +1,16 @@
-import { Projet } from "./projet.model";
 import { Sprint } from "./sprint.model";
+import { Task } from "./task.model";
 
 export class Story{
 
     public id: number;
-    public name: string;
-    public description: string;
-    public story_point: number;
-    public priorite: number;
-    public  story_of_sprint: Sprint;
-    public  story_of_projet: Projet;
+    public stReference: string;
+    public stname: string;
+    public storyPoint: number;
+    public priority: number;
+    public stdescription: string;
+    public sprint: Sprint;
+    public task: Task[];
 
 
 }
--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,15 +1,16 @@
 import { ETask } from "./etask.enum";
 import { Story } from "./story.model";
-import { User } from "./user.model";
+
 
 export class Task {
 
     public id: number;
-    public name: string;
-    public description: string;
-    public sdate_debut: Date;
-    public sdate_fin: Date;
-    public estimation: number;
+    public tReference: string;
+    public tname: string;
+    public tdescription: string;
+    public testimation: number;
+    public tdateDebut: Date;
+    public tdateFin: Date;
     public statut: ETask ;
     public story: Story;
 }
