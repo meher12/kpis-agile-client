1c855cf7ae3f383869793bba3d6819fc6046cc06
meher12 <meher.khemiri@gmail.com>
2022-03-29 15:55:42 +0200

    add taskDetails component

---
+++ src/app/component/task/task-details/task-details.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task/task.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-task-details',
+  templateUrl: './task-details.component.html',
+  styleUrls: ['./task-details.component.scss']
+})
+export class TaskDetailsComponent implements OnInit {
+
+  isLoggedIn = false;
+  showPOBoard = false;
+  showScrumMBoard = false;
+  roles: string[] = [];
+  msgError = "";
+
+  id: number;
+  task: Task;
+
+  constructor(private taskService: TaskService,  private route: ActivatedRoute,
+    private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showPOBoard = this.roles.includes('ROLE_PRODUCTOWNER');
+      this.showScrumMBoard = this.roles.includes('ROLE_SCRUMMASTER');
+
+      this.task = new Task();
+      this.id = this.route.snapshot.params['id'];
+      this.taskService.getTaskById(this.id)
+        .subscribe(data => {
+          this.task = data;
+          
+         // console.log(this.task);
+
+        },
+          err => {
+           this.msgError = err.error.message;
+           Swal.fire('Hey!', this.msgError, 'warning');
+          console.error(this.msgError);
+          });
+    }
+  }
+
+}
