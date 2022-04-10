218955935d4caa2ea5519f9f9cc1f328c4a7cf8d
meher12 <meher.khemiri@gmail.com>
2022-04-10 16:04:36 +0200

    add radar chart done

--- src/app/charts/radar-sattus-task-chart/radar-sattus-task-chart.component.ts
+++ src/app/charts/radar-sattus-task-chart/radar-sattus-task-chart.component.ts
@@ -13,6 +13,9 @@ import {
   ApexYAxis,
   ApexMarkers
 } from "ng-apexcharts";
+import { Projet } from 'src/app/models/projet.model';
+import { ProjectService } from 'src/app/services/projects/project.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -36,16 +39,93 @@ export type ChartOptions = {
 })
 export class RadarSattusTaskChartComponent implements OnInit {
 
-  @ViewChild("chart") chart: ChartComponent;
+  @ViewChild("radarChart") chart: ChartComponent;
   public chartOptions: Partial<any>;
 
-  constructor() { 
+
+
+  projects: Projet[];
+
+  keyArr: any[] = [];
+  valueArr: any[] = [];
+
+
+  isLoggedIn = false;
+  showPOBoard = false;
+  showScrumMBoard = false;
+
+  roles: string[] = [];
+
+  msgError = "";
+  selected;
+
+  constructor(private projectService: ProjectService, private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showPOBoard = this.roles.includes('ROLE_PRODUCTOWNER');
+      this.showScrumMBoard = this.roles.includes('ROLE_SCRUMMASTER');
+
+      this.getAllproject();
+
+    }
+
+  }
+
+  getAllproject() {
+    this.projectService.getProjectList()
+      .subscribe(data => {
+        this.projects = data;
+      },
+        err => {
+          this.msgError = err.error.message;
+          console.error(this.msgError);
+        });
+  }
+
+
+
+  getStatuslist(event: any) {
+    this.projectService.getListtaskByStatus(event.target.value)
+      .subscribe(
+        {
+          next: (data) => {
+            const { KeyArr, ValueArr } = data;
+
+            this.keyArr = KeyArr
+            this.valueArr = ValueArr
+            //console.log("two arrays", this.keyArr);
+            //console.log("**** rays", this.valueArr);
+
+            let valArrtoNumber = this.valueArr.map(i => Number(i));
+            //console.log(valArrtoNumber);
+
+            this.radarChart(this.keyArr, valArrtoNumber)
+          },
+
+          error: err => console.error(err),
+          //complete: () => console.log('DONE!')
+
+
+        })
+
+  }
+
+
+
+  radarChart(statusTask: any[], countStatus: any[]) {
 
     this.chartOptions = {
       series: [
         {
-          name: "Series 1",
-          data: [7, 25, 30, 15, 2, 20]
+         // name: "Series 1",
+          data: countStatus
         }
       ],
       chart: {
@@ -64,11 +144,10 @@ export class RadarSattusTaskChartComponent implements OnInit {
               colors: ["#f8f8f8", "#fff"]
             }
           }
+          
         }
       },
-      title: {
-        text: "Radar with Polygon Fill"
-      },
+
       colors: ["#FF4560"],
       markers: {
         size: 4,
@@ -78,26 +157,30 @@ export class RadarSattusTaskChartComponent implements OnInit {
       },
       tooltip: {
         y: {
-          formatter: function(val) {
+          formatter: function (val) {
             return val;
           }
         }
       },
       xaxis: {
-        categories: [
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          
-        ]
+        categories: statusTask,
+        labels: {
+          show: true,
+          style: {
+            colors: [],
+              fontSize: '15px',
+              fontFamily: 'Helvetica, Arial, sans-serif',
+              fontWeight: 400,
+              cssClass: 'apexcharts-xaxis-label',
+          }, 
+      },
+
+
       },
       yaxis: {
         tickAmount: 7,
         labels: {
-          formatter: function(val, i) {
+          formatter: function (val, i) {
             if (i % 2 === 0) {
               return val;
             } else {
@@ -105,11 +188,37 @@ export class RadarSattusTaskChartComponent implements OnInit {
             }
           }
         }
-      }
+      },
+      title: {
+        text: "Radar Status of task",
+        align: 'left',
+        margin: 30,
+        offsetX: 0,
+        offsetY: 0,
+        floating: false,
+        style: {
+          fontSize: '24px',
+          fontWeight: 'bold',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          color: '#263238'
+        },
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
     };
   }
 
-  ngOnInit(): void {
-  }
+
 
 }
