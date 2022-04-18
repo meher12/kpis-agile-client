ce8e4e15b9b8c9dac8d1f00f9cd3c11c531db52b
meher12 <meher.khemiri@gmail.com>
2022-04-18 17:38:57 +0200

    update jacoco file uploaded

---
+++ src/app/models/jacoco.model.ts
@@ -0,0 +1,10 @@
+export class JacocoReport {
+
+    public projectname: string;
+    public type: any;
+    public covered: any;
+    public missed: any;
+    public percentage: any;
+    public totalpercentage: any;
+    public createdAt: Date;
+}
---
+++ src/app/services/jacoco-report.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppConstants } from './AppConstants';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class JacocoReportService {
+
+ 
+  baseUrl: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = AppConstants.baseUrl;
+  }
+
+  // get all report
+  getReportList(): Observable<any[]> {
+    return this.httpClient.get<any[]>(`${this.baseUrl}` + '/reports/jcoverages', httpOptions);
+  }
+
+   // get all report
+   getReportListByPName(projectname: string): Observable<any[]> {
+    return this.httpClient.get<any[]>(`${this.baseUrl}` + '/reports/jcoverages/'+`${projectname}`, httpOptions);
+  }
+
+
+  // add new jacoco report
+  createReport(report: any): Observable<any> {
+    return this.httpClient.post(`${this.baseUrl}` + '/reports/add', report, httpOptions);
+  }
+}
